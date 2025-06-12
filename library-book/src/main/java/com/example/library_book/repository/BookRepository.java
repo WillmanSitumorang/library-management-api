fbf48e1128bdee18169c70d1f8588dc88aa003b9
fifken <fifken69@gmail.com>
2025-06-12 15:25:14 +0700

    repo + app

--- library-book/src/main/java/com/example/library_book/repository/BookRepository.java
+++ library-book/src/main/java/com/example/library_book/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.example.library_book.repository;
 
-public interface BookRepository {
+import com.example.library_book.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+//    List<Book> findByTitleContaining(String keyword);
 }
