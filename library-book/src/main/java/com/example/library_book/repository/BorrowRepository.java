ca8d03c86db4b9b9185c8b78f70d1c32cbbf0e7e
Putra Adhli Falah <71480@fif.co.id>
2025-06-13 10:33:03 +0700

    fix borrowRepo dan MemberService

--- library-book/src/main/java/com/example/library_book/repository/BorrowRepository.java
+++ library-book/src/main/java/com/example/library_book/repository/BorrowRepository.java
@@ -9,9 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface BorrowRepository extends JpaRepository<Borrow, Long> {
-<<<<<<< HEAD
     Optional<Borrow> findByMemberIdAndBookId(Long memberId, Long bookId);
-=======
->>>>>>> 50cbae731c7c41fa8713250cf80691889e59a0cc
     List<Borrow> findByMemberId(Long memberId);
 }
